76e5c7f2e72a5ddf09ca0f56fc7e1fd76d69c478
vighnesh-096 <vahasabe2004@gmail.com>
2026-01-31 10:33:13 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,18 @@
+#  1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# 	Addition
+# 	Subtraction
+# 	Multiplication
+# 	Division
+# 3.  Displays the results of each operation on the screen.
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the secound number: "))
+addition = num1 + num2
+print("Addition: ", addition)
+subtraction = num1 - num2
+print("Subtraction: ", subtraction)
+multiplication = num1 * num2
+print("Multiplication: ", multiplication)
+division = num1 / num2
+print("Division: ", division)
---
+++ Task2.py
@@ -0,0 +1,7 @@
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print(first_name + " " + last_name)
+print("Hello, " + first_name + " " + last_name + " Welcome!")
